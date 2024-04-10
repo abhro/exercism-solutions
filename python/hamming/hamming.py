b1277328eff98ac53c34cd32d5af97f893f81ce1
abhro <5664668+abhro@users.noreply.github.com>
2024-04-09 22:53:04 -0400

    Add docstring and type annotations for hamming.py

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,5 @@
 
-def distance(str1, str2):
+def distance(str1: str, str2: str) -> int:
+    """Determine Hamming distance between str1 and str2"""
     assert len(str1) == len(str2)
     return sum(c1 != c2 for c1, c2 in zip(str1, str2))
